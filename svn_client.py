62d766e895ce6b2e019a6f8af53c52bc272fa971
hazeez <hafizul.azeez@gmail.com>
2014-12-30 16:04:56 +0530

    moving from home code base - client code

---
+++ svn_client.py
@@ -0,0 +1,12 @@
+import svn_interface as svni
+import sys
+
+svn_path = 'http://10.184.152.13:18080/svn/FCUBS_12.0.2.0.0/branches/FCUBS_12.0.2.0.0SCOBAN_R1'
+
+# svn_path = 'http://10.184.152.13:18080/svn/FCUBS_12.0.2.0.0/'
+
+svni_tools = svni.svn_interface_tools(svn_path)
+# print svni_tools.getPathLog(svn_path)
+svni_tools.getItems(svn_path, True)
+# svni_tools.getHeadRevision(svn_path)
+
