f57bc87071fffbeddcb96513b946a5fb94ff510b
unknown <hazeez@HAZEEZ-IN.in.oracle.com>
2014-12-31 16:30:09 +0530

    added younger revision, finding tag path, generating tag list

--- svn_client.py
+++ svn_client.py
@@ -3,13 +3,20 @@ import sys
 
 svn_path = 'http://10.184.152.13:18080/svn/FCUBS_12.0.2.0.0/branches/FCUBS_12.0.2.0.0SCOBAN_R1'
 
-# svn_path = 'http://10.184.152.13:18080/svn/FCUBS_12.0.2.0.0/'
+#svn_path = 'http://10.184.152.13:18080/svn/FCUBS_12.0.2.0.0/tags'
+
+#svn_path = "http://10.184.152.13:18080/svn/FCUBS_12.0.2.0.0/branches/FCUBS_12.0.2.0.0MELOCAL_R2"
 
 svni_tools = svni.svn_interface_tools(svn_path)
 # print svni_tools.getPathLog(svn_path)
-svni_tools.getItems(svn_path, True)
+svni_tools.getItems(svn_path, False)
 # svni_tools.getHeadRevision(svn_path)
-
 # Get youngest revision
 svni_tools.getYoungestRevision(svn_path)
+
+svni_tools.generateDSUMPath(svn_path)
+
+print svni_tools.generateTagList(svn_path)
+
+
 
--- svn_interface.py
+++ svn_interface.py
@@ -1,7 +1,7 @@
 import pysvn
 from datetime import datetime
 
-repo_path = 'http://10.184.152.13:18080/svn/FCUBS_12.0.2.0.0/branches/FCUBS_12.0.2.0.0SCOBAN_R1'
+path = 'http://10.184.152.13:18080/svn/FCUBS_12.0.2.0.0/branches/FCUBS_12.0.2.0.0SCOBAN_R1'
 
 
 class svn_exception(Exception):
@@ -31,13 +31,13 @@ class svn_interface_tools:
     def getItems(self, path, recurse=False):
         ''' Returns list of tuple of information for each files in the given path'''
         try:
-            entries = pysvn.Client().list(path, recurse=recurse)
+            entries = self.client.list(path, recurse=recurse)
             # remove parent directory from the list
             del entries[0]
-            self.formatItems(entries)
+            #self.formatItems(entries)
         except pysvn.ClientError, e:
             raise svn_exception('Cannot retrieve the information from path: +path')
-        return
+        return entries
 
 # FORMAT ITEMS
     def formatItems(self, entries):
@@ -78,9 +78,8 @@ class svn_interface_tools:
         try:
             logfile = self.client.log(path)
             for x in logfile:
-                print x["revision"].number, " ", x["author"], " ", x["message"]
+                x["revision"].number, " ", x["author"], " ", x["message"]
                 no_of_revisions += 1
-            print no_of_revisions
             # TODO GET CHANGE PATH INFORMATION
         except Exception, e:
             raise svn_exception("Cannot retrieve log information for the path")
@@ -90,25 +89,69 @@ class svn_interface_tools:
     def getHeadRevision(self,path):
         try:
             infofile = self.client.revpropget("revision", url=path)
-            print infofile
         except Exception, e:
             raise svn_exception("Cannot retrieve path information")
-
+        return infofile
 
 # GET YOUNGER REVISION OF A PATH
     def getYoungestRevision(self, path):
         revision_numbers = []
         try:
             pathlog = self.client.log(path)
-            for x in logfile:
-                revision_numbers.append(x["revision"].number)
-            print revision_numbers[-1]
+            # Get youngest revision
+            gyr = pathlog[-1]["revision"].number
         except Exception, e:
             raise svn_exception("Cannot find the youngest revision of the path")
-
-
+        return gyr
 # GET LAST REVISION OF A path
 
 
-# GENERATE PATHS - BRANCHES PATH, TAG PATH, DOCS PATH, SOFT PATH, USER MANUALS PATH
+# GENERATE PATHS - DOCS PATH, SOFT PATH, USER MANUALS PATH
 
+    def generateDSUMPath(self, path):
+        try:
+            docs_path = soft_path = um_path = 0
+            entries = self.client.list(path)
+            # remove parent directory from the list
+            del entries[0]            
+            for x in entries:
+                if x[0]["path"].split("/")[-1].lower() == "docs":
+                    docs_path = x[0]["path"]
+                if x[0]["path"].split("/")[-1].lower() == "soft":
+                    soft_path = x[0]["path"]
+                if x[0]["path"].split("/")[-1].lower() == "usermanuals":
+                    um_path = x[0]["path"]
+        except Exception, e:
+            raise svn_exception("Cannot generate the directory paths")
+        return docs_path, soft_path, um_path
+
+#GENERATE TAG LIST FROM TAG PATH FOR A RELEASE
+    def generateTagList(self, path):
+        try:
+            base_tag_path = self.generateTagPath(path)
+            tags = self.getItems(base_tag_path, False)
+            release_name = self.getReleaseName(path)
+            tag_list = []
+            for x in tags:
+                tag_info = x[0]["path"]
+                if release_name in tag_info:
+                    tag_list.append(tag_info)
+            if len(tag_list) > 1:
+                tag_list.reverse()
+        except Exception, e:
+            raise svn_exception("Cannot generate the tag path")
+        return tag_list
+
+# GENERATE TAG PATH FROM BRANCHES PATH
+    def generateTagPath(self, path):
+        tag_path = ""
+        branch_path = path
+        main_tag_path = branch_path.split("/")[0:-2]
+        for x in main_tag_path:
+            tag_path += x + "/"
+        tag_path += "tags/"
+        return tag_path
+
+    def getReleaseName(self, path):
+        release_name = path.split("/")[-1]
+        return release_name 
